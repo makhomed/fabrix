af1a24497775a85a3426c43713433bd17a4255fe
Gena Makhomed <makhomed@gmail.com>
2017-10-21 22:37:01 +0300

    add new tests

--- tests/test_editor.py
+++ tests/test_editor.py
@@ -1,4 +1,5 @@
-from fabrix.editor import edit_text, append_line
+import pytest
+from fabrix.editor import edit_text, append_line, _apply_editors
 
 
 def test_append_line():
@@ -11,3 +12,15 @@ def test_append_line():
     assert edit_text("\n", append_line("append", True)) == "\n\nappend\n"
     assert edit_text("line", append_line("append", True)) == "line\n\nappend\n"
     assert edit_text("line\n", append_line("append", True)) == "line\n\nappend\n"
+
+
+def test_non_idempotend_editors():
+    def editor(text):
+        return text + "x"
+    with pytest.raises(SystemExit, message="editors is not idempotent"):
+        _apply_editors("text", editor)
+
+
+def test_empty_list_of_editors():
+    with pytest.raises(SystemExit, message="editors can\'t be empty"):
+        _apply_editors("text")
---
+++ tests/test_render.py
@@ -0,0 +1,29 @@
+import pytest
+from fabric.api import env
+from fabrix.api import render_template
+
+
+def test_render_template(tmpdir):
+    fabfile_dir = tmpdir
+    templates_dir = fabfile_dir.mkdir("templates")
+    template_file = templates_dir.join("hello.txt.j2")
+    template_file.write("Hello, {{ name}}!")
+    env.real_fabfile = str(fabfile_dir.join("fabfile.py"))
+    assert render_template("hello.txt.j2", name="World") == "Hello, World!"
+
+
+def test_render_template_failed_if_templates_dir_not_exists(tmpdir):
+    fabfile_dir = tmpdir
+    templates_dir = str(fabfile_dir.join("templates"))
+    env.real_fabfile = str(fabfile_dir.join("fabfile.py"))
+    with pytest.raises(SystemExit, message='render_template: templates dir \'%s\' not exists' % templates_dir):
+        assert render_template("hello.txt.j2", name="World") == "Hello, World!"
+
+
+def test_render_template_failed_if_template_file_not_exists(tmpdir):
+    fabfile_dir = tmpdir
+    templates_dir = fabfile_dir.mkdir("templates")
+    template_file = templates_dir.join("hello.txt.j2")
+    env.real_fabfile = str(fabfile_dir.join("fabfile.py"))
+    with pytest.raises(SystemExit, message='render_template: template \'%s\' not exists' % template_file):
+        render_template("hello.txt.j2", name="World")
