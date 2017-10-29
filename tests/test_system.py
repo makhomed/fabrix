70d141fd8ad618fa3172c775d732f4bb67ed19dd
Gena Makhomed <makhomed@gmail.com>
2017-10-29 10:44:26 +0200

    renamed reboot.py to system.py

--- tests/test_system.py
+++ tests/test_system.py
@@ -1,8 +1,8 @@
 import time
-import fabrix.reboot
+import fabrix.system
 from conftest import mock_run_factory
 from fabric.api import env
-from fabrix.reboot import is_reboot_required, reboot_and_wait
+from fabrix.system import is_reboot_required, reboot_and_wait
 
 
 def test_is_reboot_required(monkeypatch):
@@ -12,14 +12,14 @@ def test_is_reboot_required(monkeypatch):
         r'/usr/bin/needs-restarting -r': {'stdout': '', 'failed': False},
     }
     mock_run = mock_run_factory(run_state)
-    monkeypatch.setattr(fabrix.reboot, 'run', mock_run)
+    monkeypatch.setattr(fabrix.system, 'run', mock_run)
     assert is_reboot_required() is False
     run_state = {
         r'if \[ ! -e /usr/bin/needs-restarting \] ; then echo notexists ; fi': {'stdout': '', 'failed': False},
         r'/usr/bin/needs-restarting -r': {'stdout': 'Reboot is required to ensure that your system benefits from these updates.', 'failed': False},
     }
     mock_run = mock_run_factory(run_state)
-    monkeypatch.setattr(fabrix.reboot, 'run', mock_run)
+    monkeypatch.setattr(fabrix.system, 'run', mock_run)
     assert is_reboot_required() is True
 
 
@@ -29,7 +29,7 @@ def test_reboot_and_wait(monkeypatch):
         r'reboot': {'stdout': '', 'failed': False},
     }
     mock_run = mock_run_factory(run_state)
-    monkeypatch.setattr(fabrix.reboot, 'run', mock_run)
-    monkeypatch.setattr(fabrix.reboot.connections, 'connect', lambda x: None)
+    monkeypatch.setattr(fabrix.system, 'run', mock_run)
+    monkeypatch.setattr(fabrix.system.connections, 'connect', lambda x: None)
     monkeypatch.setattr(time, 'sleep', lambda x: None)
     assert reboot_and_wait() is None
