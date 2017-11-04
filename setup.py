c1e650e9da5358763b84d511509c8811023be105
Gena Makhomed <makhomed@gmail.com>
2017-11-04 18:56:41 +0200

    update long_description

--- setup.py
+++ setup.py
@@ -4,9 +4,13 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
+
+lines = list()
 here = path.abspath(path.dirname(__file__))
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
+with open(path.join(here, 'README.rst'), encoding='utf-8') as readme_rst:
+    lines = [line.rstrip('\n') for line in readme_rst]
+lines=lines[4:]
+long_description = "\n".join(lines)
 
 setup(
     name='Fabrix',
